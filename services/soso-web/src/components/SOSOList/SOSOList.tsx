3cb36edf42cc91ae1397524b591d1159fed34003
taminororo <169162271+taminororo@users.noreply.github.com>
2025-08-04 23:37:29 +0900

    [feat]SOSOListコンポーネントの作成

---
+++ services/soso-web/src/components/SOSOList/SOSOList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SOSOcard from './SOSOListCard'; // SOSOcardコンポーネントをインポート
+
+// SOSOポイント変動履歴のデータの型を定義
+export interface SOSOPointLog {
+  id: number | string;
+  eventName: string; // 画像のようにイベント名も表示
+  date: string;
+  time: string;
+  changer: string;
+  changee: string;
+  sosoPoints: number;
+  reason: string;
+}
+
+// SOSOListが受け取るpropsの型
+interface SOSOListProps {
+  logs: SOSOPointLog[]; // SOSOPointLogの配列
+}
+
+function SOSOList({ logs }: SOSOListProps) {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* logs配列をループして、SOSOcardを生成 */}
+      {logs.map((log) => (
+        <SOSOcard
+          key={log.id} // 各要素にユニークなkeyを指定
+          date={log.date}
+          time={log.time}
+          changer={log.changer}
+          changee={log.changee}
+          sosoPoints={log.sosoPoints}
+          reason={log.reason}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default SOSOList;
