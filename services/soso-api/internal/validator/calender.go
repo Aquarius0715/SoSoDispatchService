b70e0a1ca96196760aec8c9a0c4d2e8e22ff430b
ji1wxs <ji1wxs@outlook.jp>
2025-08-04 23:16:12 +0900

    [add] calender resister validationを追加

---
+++ services/soso-api/internal/validator/calender.go
@@ -0,0 +1,10 @@
+package validator
+
+import "github.com/go-playground/validator/v10"
+
+func ResisterCalender(v *validator.Validate) {
+	_ = v.RegisterValidation("name", func(fl validator.FieldLevel) bool {
+		l := len(fl.Field().String())
+		return l < 128
+	})
+}
