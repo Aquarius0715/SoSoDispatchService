111c8ea09f92b00c271206d920276a059e6970b2
ji1wxs <ji1wxs@outlook.jp>
2025-08-04 23:02:11 +0900

    [add] find calender by namerepositoryを追加

--- services/soso-api/internal/repository/calender.go
+++ services/soso-api/internal/repository/calender.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"soso/internal/model"
 )
 
@@ -12,6 +13,32 @@ type CalenderRepository struct {
 
 func NewCalenderRepository(db *sql.DB) *CalenderRepository {
 	return &CalenderRepository{DB: db}
+}
+
+func (r *CalenderRepository) FindByName(ctx context.Context, name string) (*model.Calender, error) {
+	row := r.DB.QueryRowContext(ctx, `
+		SELECT
+			id,
+			name,
+			description,
+			owner_id,
+			created_at,
+			updated_at
+		FROM calenders
+		WHERE name = ?
+		LIMIT 1
+	`, name)
+
+	var c model.Calender
+	if err := row.Scan(
+		&c.ID, &c.Name, &c.Description, &c.OwnerId, &c.CreatedAt, &c.UpdatedAt,
+	); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &c, nil
 }
 
 func (r *CalenderRepository) Create(ctx context.Context, c *model.Calender) error {
