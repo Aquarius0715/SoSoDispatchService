d7d44b60b6c59b6a6447ea0cb4128a58324d46c0
taminororo <169162271+taminororo@users.noreply.github.com>
2025-08-06 01:41:33 +0900

    initial commit

---
+++ services/soso-web/src/components/Modal/EventAddModal.tsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+import clsx from 'clsx';
+import { Member } from '../MemberList/MenberList';
+import Textarea from '../TextFieald/Textfieald';
