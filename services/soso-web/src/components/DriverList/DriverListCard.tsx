a04431db62ac765dc28fd1255c6102c0954d559f
makochannnnnn <25.m.yamamoto.nutfes@gmail.com>
2025-08-06 23:53:55 +0900

    SriverListCardのスペルミス修正

--- services/soso-web/src/components/DriverList/DriverListCard.tsx
+++ services/soso-web/src/components/DriverList/DriverListCard.tsx
@@ -20,7 +20,7 @@ function DriverListCard({className,drive,children}: Props) {
 
   return (
     <div className={clsx('flex flex-col bg-primary-1 rounded-lg', finalClassName)}>
-      <p className='text-sm text-gray-600'>{drive.calenderName}</p>
+      <p className='text-sm text-gray-600'>{drive.calendarName}</p>
       <p className='font-medium text-gray-800'>{drive.eventName}</p>
       <p className='text-sm text-gray-600'>{drive.eventDate} {drive.driveTime}</p>
       <p className='text-sm text-gray-600'>{drive.driveState !== undefined && driveStateText[drive.driveState]}: {drive.passengerNumber}</p>
