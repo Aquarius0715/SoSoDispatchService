7424284d634c8d9ef663c6b58102b0434f68b214
ji1wxs <ji1wxs@outlook.jp>
2025-08-07 20:32:41 +0900

    [add] event handlerのテストを追加

--- services/soso-api/tests/internal/handlers/helpers_test.go
+++ services/soso-api/tests/internal/handlers/helpers_test.go
@@ -204,4 +204,52 @@ const (
 		INNER JOIN users AS u ON u.id = cm.user_id
 		WHERE cm.calender_id = ?
 	`
+	SQLEventCreate = `
+		INSERT INTO events (
+			id,
+			calender_id,
+			creator_id,
+			title,
+			description,
+			start_time,
+			end_time,
+			origin_location,
+			destination_location,
+			seats_required
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+	SQLEventFindByCalenderID = `
+		SELECT
+			id,
+			calender_id,
+			creator_id,
+			title,
+			description,
+			start_time,
+			end_time,
+			origin_location,
+			destination_location,
+			seats_required,
+			created_at,
+			updated_at
+		FROM events
+		WHERE calender_id = ?`
+
+	SQLEventFindById = `
+		SELECT
+			id,
+			calender_id,
+			creator_id,
+			title,
+			description,
+			start_time,
+			end_time,
+			origin_location,
+			destination_location,
+			seats_required,
+			created_at,
+			updated_at
+		FROM events
+		WHERE id = ?
+		LIMIT 1`
 )
