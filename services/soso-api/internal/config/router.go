cf473b2c799d48da9e55e624a04d4216cdfe8485
ji1wxs <ji1wxs@outlook.jp>
2025-08-09 00:30:20 +0900

    [fix] routingを変更

--- services/soso-api/internal/config/router.go
+++ services/soso-api/internal/config/router.go
@@ -126,11 +126,11 @@ func SetupRouter(cfg *Config) *echo.Echo {
 	calG.GET("/:calender_id/events", eventH.ListByCalender)
 
 	eveG := e.Group("/events", csrfMW, echojwt.WithConfig(jwtCfg))
-	eveG.GET(":event_id", eventH.FindById)
-	eveG.POST(":event_id/pickup", eventH.RegisterPickUp)
-	eveG.POST(":event_id/return", eventH.RegisterReturn)
-	eveG.GET(":event_id/detail", eventH.Detail)
-	eveG.GET(":event_id/members", eventH.Members)
+	eveG.GET("/:event_id", eventH.FindById)
+	eveG.POST("/:event_id/pickup", eventH.RegisterPickUp)
+	eveG.POST("/:event_id/return", eventH.RegisterReturn)
+	eveG.GET("/:event_id/detail", eventH.Detail)
+	eveG.GET("/:event_id/members", eventH.Members)
 
 	// シャットダウン時クローズ
 	e.Server.RegisterOnShutdown(func() { _ = db.Close() })
