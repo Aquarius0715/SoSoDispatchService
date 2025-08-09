0f4181c53eb7bd09631e7ee8862b4af8b8103098
ji1wxs <ji1wxs@outlook.jp>
2025-08-09 14:33:25 +0900

    [fix] ルーティングURIを変更

--- services/soso-api/internal/config/router.go
+++ services/soso-api/internal/config/router.go
@@ -124,7 +124,7 @@ func SetupRouter(cfg *Config) *echo.Echo {
 	calG.POST("/:calender_id/join", calenderMembershipH.Create)
 	calG.GET("/my", calenderH.FindMyCalenders)
 	calG.GET("/:calender_id/members", calenderMembershipH.List)
-	calG.PUT("/:calender_id/members/:user_id", sosoPH.Update)
+	calG.PUT("/:calender_id/members/:user_id/point", sosoPH.Update)
 	calG.POST("/:calender_id/events", eventH.Create)
 	calG.GET("/:calender_id/events", eventH.ListByCalender)
 
