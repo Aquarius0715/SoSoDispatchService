6f0373d4cf3753b12a78276ad47d2992148a91c7
taminororo <169162271+taminororo@users.noreply.github.com>
2025-09-03 15:09:40 +0900

    [fix]入力処理の削除

--- services/soso-web/src/components/Modal/EventDetailModal.tsx
+++ services/soso-web/src/components/Modal/EventDetailModal.tsx
@@ -394,23 +394,6 @@ const EventDetailModal: React.FC<ModalProps> = ({ eventData, onClose }) => {
         </div>
 
         <div className="text-zinc-600">
-          <h3 className="font-normal font-['Inter'] text-black text-lg mb-2">配車登録</h3>
-          <div className="flex items-center mb-4">
-            <p className="text-sm mr-2">乗車可能人数</p>
-            <Input
-              className="w-1/4 mr-2"
-              type="number"
-              value={seatsRequired}
-              onChange={(e) => {
-                const value = e.target.value;
-                if (value === '' || (!isNaN(Number(value)) && Number(value) >= 0)) {
-                  setSeatsRequired(value === '' ? '' : Number(value));
-                }
-              }}
-              disabled={isRegistering} // ★ 登録中は無効化
-            />
-            <span className="text-sm">人</span>
-          </div>
           <div className="flex space-x-4">
             <Button
               className={`px-0 py-4 flex-1 text-white ${
