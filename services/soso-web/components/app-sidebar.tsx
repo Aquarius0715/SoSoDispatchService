20e1e4741cb1d8fd29da7ded0243c9607be50c54
taminororo <169162271+taminororo@users.noreply.github.com>
2025-11-01 11:51:20 +0900

    [add]shadcnの導入

---
+++ services/soso-web/components/app-sidebar.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import * as React from "react"
+import { Sidebar } from "@/components/ui/sidebar"
+
+export function AppSidebar(): React.ReactElement {
+  return (
+    <Sidebar>
+      <div />
+    </Sidebar>
+  )
+}
+
+
