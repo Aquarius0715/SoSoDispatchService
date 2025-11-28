e9a5e9e5bd5743f0e5b66e5a9a4dde972e681c49
taminororo <169162271+taminororo@users.noreply.github.com>
2025-11-28 22:31:39 +0900

    [add]やるぞ宣言

--- services/soso-web/src/app/dashboard/page.tsx
+++ services/soso-web/src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
-
+import { AppSidebar } from "@/components/dashboard-page/right-sidebar-view/app-sidebar"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider className="flex-row-reverse">
