5d9056818c6bfdaafe26eadf6a95c87cb9f9660a
taminororo <169162271+taminororo@users.noreply.github.com>
2026-04-09 08:29:29 +0900

    feat: 型定義をバックエンドと合わせる

--- services/soso-web/src/types/interfaces.ts
+++ services/soso-web/src/types/interfaces.ts
@@ -27,6 +27,32 @@ export type CalenderMember = {
   sosoPoint: number;
 };
 
+export type Driver = {
+  username: string;
+  capacity: number;
+};
+
+
+export type EventDetail = {
+  title: string;
+  startTime: string;
+  endTime: string;
+  description: string;
+  originLocation: string
+  destinationLocation: string;
+  seatsRequiredGo:  number;
+  seatsRequiredReturn: number;
+  remainingGoSeats: number;
+  remainingReturnSeats: number;
+  participants: string[];
+  goDrivers: Driver[];
+  returnDrivers: Driver[];
+  goCapacityTotal: number;
+  returnCapacityTotal: number;
+
+
+};
+
 export type SosoPointHistory = {
   id: number;
   calenderId: string;
